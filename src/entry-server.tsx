621ce310a3ed8fa6444f3eac7f788041dd83fc8a
Jérémy Chatard <jchatard@breek.fr>
2023-04-23 19:34:43 +0200

    Add Sentry example code.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -4,6 +4,31 @@ import {
   StartServer,
 } from "solid-start/entry-server";
 
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: "https://your-ingestion-url.sentry.io",
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
+
 export default createHandler(
   renderAsync((event) => <StartServer event={event} />)
 );
